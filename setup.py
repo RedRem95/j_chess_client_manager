a33174904d02b85a8d0604a4e594065d57d16fa5
RedRem95 <redrem_github@botschmot.de>
2022-01-31 22:18:06 +0100

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/RedRem95/j_chess_client_manager',
-    version='0.1.2',
+    version='0.1.3',
     zip_safe=False,
 )
